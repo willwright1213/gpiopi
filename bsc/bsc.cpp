84951a74619d0dda343e7d71f2912d531221df96
William Wright <williamwright3@cmail.carleton.ca>
2021-09-14 01:29:30 +0100

    "updated bsc.cpp"

--- bsc/bsc.cpp
+++ bsc/bsc.cpp
@@ -22,27 +22,27 @@ BSC::BSC(std::uint8_t sa)
 std::int8_t BSC::enable_i2c()
 {
     addr[C] |= (1 << 15);
-    return msync(addr, 32, MS_SYNC);
+    return 0;
 }
 
 std::int8_t BSC::disable_i2c()
 {
     addr[C] &= ~(1 << 15);
-    return msync(addr, 32, MS_SYNC);
+    return 0;
 }
 
 // Set the Control register to writing mode
 std::int8_t BSC::set_write()
 {
     addr[C] &= ~(1); //set writing mode
-    return msync(addr, 32, MS_SYNC);
+    return 0;
 }
 
 // Set the Control register to reading mode
 std::int8_t BSC::set_read()
 {
     addr[C] |= (1 << 0);
-    return msync(addr, 32, MS_SYNC);
+    return 0;
 }
 
 // Set the Control regiser to clear.
@@ -50,7 +50,7 @@ std::int8_t BSC::set_read()
 std::int8_t BSC::set_clear()
 {
     addr[C] |= (1 << 4);
-    return msync(addr, 32, MS_SYNC);
+    return 0;
 }
 
 // Will start the transfer if i2c is enabled or wait if disabled.
@@ -58,7 +58,7 @@ std::int8_t BSC::set_clear()
 std::int8_t BSC::start()
 {
     addr[C] |= (1 << 7);
-    return msync(addr, 32, MS_SYNC);
+    return 0;
 }
 
 // Clear and start can be set at the same time.
@@ -69,7 +69,7 @@ std::int8_t BSC::clear_start()
     if((addr[C] >> 15)&1 == 1) return 0;
     addr[C] |= (1 << 4);
     addr[C] |= (1 << 7);
-    return msync(addr, 32, MS_SYNC);
+    return 0;
 }
 
 std::int8_t BSC::done_status() { return (addr[S] >> 1)&1; }
@@ -77,13 +77,13 @@ std::int8_t BSC::done_status() { return (addr[S] >> 1)&1; }
 std::int8_t BSC::clear_done()
 {
     addr[S] |= (1 << 1);
-    return msync(addr, 32, MS_SYNC);
+    return 0;
 }
 
 std::int8_t BSC::trans_status() { return addr[S] & 1;}
 
-void BSC::set_dlen(int16_t len) { addr[DLEN] = len; msync(addr, 32, MS_SYNC);}
-void BSC::write_fifo(int32_t data) { addr[FIFO] = data; msync(addr, 32, MS_SYNC);}
+void BSC::set_dlen(int16_t len) { addr[DLEN] = len; }
+void BSC::write_fifo(int32_t data) { addr[FIFO] = data; }
 uint32_t BSC::read_fifo() { return addr[FIFO]; }
 
 void BSC::clear_map() { munmap(addr, 32); }
